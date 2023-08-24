c19bd5878150cd2ef67129475aaf48557b30d8ef
Sneez Noodle <onesyboy@gmail.com>
2023-08-24 19:53:42 +0100

    Clean lsp.lua
    Used lspconfig's "default setup table" capabilities and vim.tbl_extend
    instead of my own extend function

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -11,30 +11,29 @@ return {
 			"hrsh7th/cmp-nvim-lsp",
 		},
 		config = function()
-			local m = require("mason-lspconfig")
-			local l = require("lspconfig")
+			-- Setup mason-lspconfig before lspconfig
+			local mason_lspconfig = require("mason-lspconfig")
+			mason_lspconfig.setup { ensure_installed = { "lua_ls" } }
 
-			m.setup { ensure_installed = { "lua_ls" } }
-			m.setup_handlers {
-				function(server_name)
-					local default_config = {
-						capabilities = require("cmp_nvim_lsp").default_capabilities(), -- Add cmp capabilities
-						on_attach = function(client, bufnr)
-							client.server_capabilities.semanticTokensProvider = nil -- Let treesitter handle syntax highlighting (except it breaks every 5 minutes)
-						end,
-					}
-
-					local has_cfg, config = pcall(require, "lsp-settings." .. server_name)
-					if not has_cfg then
-						config = default_config
-					else
-						for k, v in pairs(default_config) do
-							config[k] = config[k] or v
-						end
-					end
-
-					l[server_name].setup(config)
+			-- Set default config for lspconfig
+			local default_config = {
+				capabilities = require("cmp_nvim_lsp").default_capabilities(), -- Add cmp capabilities
+				on_attach = function(client, bufnr)
+					client.server_capabilities.semanticTokensProvider = nil -- Let treesitter handle syntax highlighting (except it breaks every 5 minutes)
 				end,
+			}
+			local lspconfig = require("lspconfig")
+			lspconfig.util.default_config = vim.tbl_extend("force", lspconfig.util.default_config, default_config)
+
+			-- Use config from respective file
+			mason_lspconfig.setup_handlers {
+				function(server_name)
+					local has_config, config = pcall(require, "lsp-settings." .. server_name)
+					if not has_config then
+						config = { } -- Use defaults
+					end
+					lspconfig[server_name].setup(config)
+				end
 			}
 
 			-- LSP support for vim.api.nvim_*
