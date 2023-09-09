19e597304f5dcbb6088ce3549b471f675ba2f172
Sneez <onesyboy@gmail.com>
2023-09-09 17:25:27 +0100

    Update fold on insert mode leave
    This really should be the default, foldexpr is completely useless
    without it

--- lua/sneez/core/autocmds.lua
+++ lua/sneez/core/autocmds.lua
@@ -2,7 +2,10 @@ return function()
 	vim.cmd([[
 	augroup Sneez
 	au!
+	" Highlight yanked text
 	au TextYankPost * silent! lua vim.highlight.on_yank { higroup = "Search" }
+	" Run foldexpr upon leaving insert mode (why is this not the default)
+	au InsertLeave * normal zx
 	augroup END
 	]])
 end
