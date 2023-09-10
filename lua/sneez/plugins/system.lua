89ed49f8ab8ecbbcf2dfe5f0531b3dbeea67c353
Sneez Noodle <onesyboy@gmail.com>
2023-09-10 19:37:47 +0100

    Clean up plugin specs
    Now all specs will either use `config` or `opts` for setup, but never
    both at once.

--- lua/sneez/plugins/system.lua
+++ lua/sneez/plugins/system.lua
@@ -9,40 +9,39 @@ return {
 	{
 		"stevearc/oil.nvim",
 		dependencies = { "nvim-tree/nvim-web-devicons" },
-		opts = {
-			win_options = {
-				number = true,
-				relativenumber = false,
-			},
-			use_default_keymaps = false,
-			keymaps = {
-				["g?"] = "actions.show_help",
-				["<F5>"] = "actions.refresh",
-				["<C-.>"] = "actions.toggle_hidden",
-
-				["<CR>"] = "actions.select",
-				["<C-s>"] = "actions.select_split",
-				["<C-v>"] = "actions.select_vsplit",
-				["<C-t>"] = "actions.select_tab",
-				["<C-p>"] = "actions.preview",
-				["<C-c>"] = "actions.close",
-
-				["-"] = "actions.parent",
-
-				["_"] = "actions.open_cwd",
-				["="] = "actions.cd",
-				["+"] = "actions.tcd",
-			},
-			float = {
-				border = "single",
-				win_options = { winblend = 0 },
-			},
-			preview = { border = "single" },
-			progress = { border = "single" },
-		},
-		config = function(plug, opts)
+		config = function()
 			local oil = require("oil")
-			oil.setup(opts)
+			oil.setup {
+				win_options = {
+					number = true,
+					relativenumber = false,
+				},
+				use_default_keymaps = false,
+				keymaps = {
+					["g?"] = "actions.show_help",
+					["<F5>"] = "actions.refresh",
+					["<C-.>"] = "actions.toggle_hidden",
+
+					["<CR>"] = "actions.select",
+					["<C-s>"] = "actions.select_split",
+					["<C-v>"] = "actions.select_vsplit",
+					["<C-t>"] = "actions.select_tab",
+					["<C-p>"] = "actions.preview",
+					["<C-c>"] = "actions.close",
+
+					["-"] = "actions.parent",
+
+					["_"] = "actions.open_cwd",
+					["="] = "actions.cd",
+					["+"] = "actions.tcd",
+				},
+				float = {
+					border = "single",
+					win_options = { winblend = 0 },
+				},
+				preview = { border = "single" },
+				progress = { border = "single" },
+			}
 
 			-- Set float toggle keymaps
 			vim.keymap.set("n", "<Leader>e", function()
--- lua/sneez/plugins/treesitter.lua
+++ lua/sneez/plugins/treesitter.lua
@@ -1,48 +1,47 @@
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
-		opts = {
-			ensure_installed = { "lua" },
-
-			highlight = { enable = true },
-			indent = { enable = true },
-			incremental_selection = {
-				enable = true,
-				keymaps = { -- Prefix mnemonic: node
-					init_selection = "<Leader>nn",
-					node_incremental = "<C-=>",
-					node_decremental = "<C-->",
-					scope_incremental = "<Leader>ns",
-				},
-			},
-			textobjects = {
-				select = {
-					enable = true,
-					lookahead = true,
-					keymaps = {
-						["af"] = "@function.outer",
-						["if"] = "@function.inner",
-						["ac"] = "@class.outer",
-						["ic"] = "@class.inner",
-					},
-				},
-				swap = {
-					enable = true,
-
-					swap_next = {
-						["<Leader>nl"] = "@parameter.inner",
-						["<Leader>nj"] = "@function.outer",
-					},
-					swap_previous = {
-						["<Leader>nh"] = "@parameter.inner",
-						["<Leader>nk"] = "@function.outer",
-					},
-				},
-			},
-		},
-		config = function(plugin, opts)
+		config = function()
 			vim.cmd("silent TSUpdate")
-			require("nvim-treesitter.configs").setup(opts)
+			require("nvim-treesitter.configs").setup {
+				ensure_installed = { "lua" },
+
+				highlight = { enable = true },
+				indent = { enable = true },
+				incremental_selection = {
+					enable = true,
+					keymaps = { -- Prefix mnemonic: node
+						init_selection = "<Leader>nn",
+						node_incremental = "<C-=>",
+						node_decremental = "<C-->",
+						scope_incremental = "<C-s>",
+					},
+				},
+				textobjects = {
+					select = {
+						enable = true,
+						lookahead = true,
+						keymaps = {
+							["af"] = "@function.outer",
+							["if"] = "@function.inner",
+							["ac"] = "@class.outer",
+							["ic"] = "@class.inner",
+						},
+					},
+					swap = {
+						enable = true,
+
+						swap_next = {
+							["<Leader>nl"] = "@parameter.inner",
+							["<Leader>nj"] = "@function.outer",
+						},
+						swap_previous = {
+							["<Leader>nh"] = "@parameter.inner",
+							["<Leader>nk"] = "@function.outer",
+						},
+					},
+				},
+			}
 		end,
 	},
 	{
