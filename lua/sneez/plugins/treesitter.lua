246c88be0b1876ebc3a64fa5e14817267788b3d4
Sneez Noodle <onesyboy@gmail.com>
2023-09-15 20:55:33 +0100

    Move TSUpdate to Lazy's build step

--- lua/sneez/plugins/treesitter.lua
+++ lua/sneez/plugins/treesitter.lua
@@ -1,8 +1,8 @@
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
 		config = function()
-			vim.cmd("silent TSUpdate")
 			require("nvim-treesitter.configs").setup {
 				ensure_installed = { "lua" },
 
