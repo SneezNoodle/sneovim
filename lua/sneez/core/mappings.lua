6af93e91322ca5b109e2987bda4c46bf73a71ef2
Sneez <onesyboy@gmail.com>
2023-12-03 18:39:14 +0000

    Add <C-w>= shortcut

--- lua/sneez/core/mappings.lua
+++ lua/sneez/core/mappings.lua
@@ -50,6 +50,7 @@ local mode_mappings = {
 		["<A-->"] = { "<C-W>-", { desc = "Shrink horizontal split" } },
 		["<A-.>"] = { "<C-W>>", { desc = "Grow vertical split" } },
 		["<A-,>"] = { "<C-W><", { desc = "Shrink vertical split" } },
+		["<A-+>"] = { "<C-W>=", { desc = "Evenly distribute screen space" } },
 
 		-- Config shortcut
 		["<F1>"] = { "<cmd>tabnew | edit " .. vim.fn.stdpath("config") .. "/lua/sneez/<cr>", { desc = "Edit config" } },
